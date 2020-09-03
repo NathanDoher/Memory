c4b871d995033de1802ca767afde51c7173d76c8
NathanDoher <32580897+NathanDoher@users.noreply.github.com>
2020-09-03 16:13:03 -0500

    Add files via upload

--- Memory.m
+++ Memory.m
@@ -10,7 +10,6 @@ if nargin ==0  %checks to see if game is just starting up and sets default to 4
     playerCount=1;
 else 
     
-    
 end
 
 a = ' ';        %Creates a place holder for temp button location
@@ -23,12 +22,11 @@ tic
 
 cellNum=randmat(matSize);%creates random matrix based off difficulty level
 
-
 S.fh = figure('units','normalized','position',[.3 .3 .5 .5],'menubar','none','name','Memory','numbertitle','off','resize','on');
 
-if p==1
-
-    y=1; 
+if p==1                  %Player 1 options and player 2 options
+    y=1;                 %Sets the ratio of the buttons to full screen
+    
 elseif p==2
     
     scoreTop={0 0};
@@ -38,14 +36,14 @@ elseif p==2
 
     S.pt_n1 = uicontrol('style','text','unit','normalized','fontsize',15,...
         'position',[.15 .9 .25 .1],'string',name{1},'backgroundc',[0.94 .94 .94],...
-        'HorizontalAlignment','left','ForegroundColor',[1 .7 .3]);
+        'HorizontalAlignment','left','ForegroundColor',name{3});
 
     S.pt_s2 = uicontrol('style','text','unit','normalized','fontsize',15,...
         'position',[.5 .9 .1 .1],'string',scoreTop{2},'backgroundc',[0.94 .94 .94]);
 
     S.pt_n2 = uicontrol('style','text','unit','normalized','fontsize',15,...
         'position',[.65 .9 .25 .1],'string',name{2},'backgroundc',[0.94 .94 .94],...
-        'HorizontalAlignment','left','ForegroundColor',[0 .5 1]);
+        'HorizontalAlignment','left','ForegroundColor',name{4});
     y=.90;               %changes board size to account for a 2 player information bar
     j=randperm(2,1);     %Starts the game with a random starting player
     
@@ -55,9 +53,8 @@ elseif p==2
     elseif j==2
         set(S.pt_s1,'Enable','off')
         set(S.pt_n1,'Enable','off')
-    end
-end
- 
+    end %if j==1
+end %if p==1
 
 w=matSize*2;             %Used to make columns for buttons
 d=1/w;                   %Used to make button location and button size
@@ -85,19 +82,14 @@ for cl=1:w
         'string','*',... %The default back of the button
         'callback',{@pb_call,cellNum{rl,cl},p},... %@pb_call, access the pb_call function; cellNum transfers the random number associated to the button to the call function, p variable is to send if the game is 1 player or 2 player. 
         'backgroundc',col,... %Sets the default color.
-        'busyaction','cancel',...
-        'interrupt','off',...
         'UserData',struct('val',v,'butLoc',i)); %Transfers the button 'group' number and the button structure name for later access.
     end %for rl=1:S.c
 end %cl=1:w
 
-
-
 %-----------------Menu options---------------------------------------------
 memMenu = uimenu(S.fh,'Label','Game');
-uimenu(memMenu,'Label','1-Player','Callback',@tryagain);
+uimenu(memMenu,'Label','1-Player','Callback',@playOne);
 uimenu(memMenu,'Label','2-Player','Callback',@playTwo);
-uimenu(memMenu,'Label','High Score','Callback',@score);
 uimenu(memMenu,'Label','Exit','Callback',@endgame);
 
 memSize = uimenu(S.fh,'Label','Difficulty');
@@ -112,7 +104,9 @@ uimenu(memHelp,'Label','About','Callback',{@about});
 
 %-----------------Push button function-------------------------------------
 function [] = pb_call(src,~,x,p) % Callback for pushbutton. src is the variable for the button structure, and x is the button value
-data = src.UserData; %stores information that is carried over by the button
+%%%%Push Button function was adapted and heavily changed from GUI_9%%%%
+    
+    data = src.UserData; %stores information that is carried over by the button
 
 if p==1 %checks to see if 1 player game or 2 player game, then checks to see whos turn it is
     turn=0;
@@ -124,7 +118,6 @@ elseif p==2
         turn=2;
     end %ck==1
 end %if p==1
-
 
 if c(1)==1 && data.val==1 %Checks to make sure 2nd button in group does not get pushed
 
@@ -154,24 +147,21 @@ elseif c(1)==0 && data.val==1 %Starts check for button in a group
                 set(S.pb(a),'backg',get(S.pt_n2,'ForegroundColor'))
             end
             
-            
             set(src,'Enable','off') %Turns off button for the match
             set(S.pb(a),'Enable','off') %Turns off button for the match
             c(2) = c(2)-1; %Subtracts the counter for group 2
             endcounter = endcounter  +1;
-            
-            
-            
 
             if endcounter == pairs %End game message, transfering if its a 1 player game or 2 player game
                 endMemGUI(p)
-            end
+            end %if endcounter == pairs
             
         else
             set(src,'str','*','backg','cyan') %Resets button because of incomplete match
             set(S.pb(a),'str','*','backg',[0 1 .5]) %Resets button because of incomplete match
             c(2) = c(2)-1; %Subtracts the counter for group 2
-            
+ 
+%%%%Adapted the Enable option from the memory game cited, in order to pass whos turn it is.%%%%
             if turn==1 %Due to wrong match, turn switches
                 set(S.pt_s1,'Enable','off')
                 set(S.pt_n1,'Enable','off')
@@ -182,9 +172,7 @@ elseif c(1)==0 && data.val==1 %Starts check for button in a group
                 set(S.pt_n2,'Enable','off')
                 set(S.pt_s1,'Enable','on')
                 set(S.pt_n1,'Enable','on')
-            end
-            
-            
+            end %if turn==1
         end %if x==b
     end %if c(2)==0
     
@@ -237,8 +225,7 @@ elseif c(2)==0 && data.val==2
                 set(S.pt_n2,'Enable','off')
                 set(S.pt_s1,'Enable','on')
                 set(S.pt_n1,'Enable','on')
-            end
-
+            end %if turn==1 
         end %end for if x==b
     end %end for c(1)==0
 end %c(1)==1 && data.val==1
@@ -249,26 +236,38 @@ function []=endMemGUI(p)
 
 accuracy=((matSize^2/counter)*100);
 
+
 if p==1  %Message for 1 player games and different message for 2 player games
     aCell={'You have completed the puzzle in', counter , 'tries and',...
         toc 'seconds','Your accuracy was', accuracy, 'percent'};
 elseif p==2
     aCell={['Player ' get(S.pt_n1,'string') ' Scored:'],get(S.pt_s1,'string'),...
         ['Player ' get(S.pt_n2,'string') ' Scored:'], get(S.pt_s2,'string'),...
-        toc 'seconds','Your accuracy was', accuracy, 'percent'};
+        'Total time played', toc, 'seconds'};
 end
-    
 
+if p==1 && matSize==3
+                NAME = input('Please Enter Your Name: ', 's');
+                Leaderboard = fopen('EasyLeaderboard.txt', 'w');
+                fprintf(Leaderboard, 'Name:            Attempts: \n');
+                fprintf(Leaderboard,'%s          %5.0f \n', NAME, counter);
+end
 
-    
+if p==1 && matSize==4 
+                NAME = input('Please Enter Your Name: ', 's');
+                Leaderboard = fopen('MediumLeaderboard.txt', 'w');
+                fprintf(Leaderboard, 'Name:            Attempts: \n');
+                fprintf(Leaderboard,'%s          %5.0f \n', NAME, counter);
+end
 
+if p==1 && matSize==6 
+                NAME = input('Please Enter Your Name: ', 's');
+                Leaderboard = fopen('HardLeaderboard.txt', 'w');
+                fprintf(Leaderboard, 'Name:            Attempts: \n');
+                fprintf(Leaderboard,'%s          %5.0f \n', NAME, counter);
+end
 
-S.fhe = figure('units','normalized',...
-              'position',[.3 .3 .3 .4],...
-              'menubar','none',...
-              'name','Game Over',...
-              'numbertitle','off',...
-              'resize','off');
+S.fhe = figure('units','normalized','position',[.3 .3 .3 .4],'menubar','none','name','Game Over','numbertitle','off','resize','off');
 
 S.pb = uicontrol('style','text','unit','normalized','fontsize',14,'position',[0 .25 1 .75 ],'string',aCell,'backgroundc',[0.94 .94 .94]); %
 
@@ -279,26 +278,83 @@ S.pb = uicontrol('style','push','unit','normalized','fontsize',14,'position',[.5
     {@endgame},'backgroundc',[0.94 .94 .94],'busyaction','cancel','interrupt','off'); %
 end %function [mat]=endMem()
 
+%-----------------One Player Options-----------------
+function [] = playOne(~,~)
+p=1;
+close all
+Memory(matSize,p,name)
+end %function [] = playOne(~,~)
+
+
 %-----------------Two Player Options-----------------
 function [] = playTwo(~,~)
-name1='Ben';
-name2='Isaac';
+
+S.fh = figure('units','normalized','position',[.3 .3 .25 .20],'menubar','none','name','Custom Options','numbertitle','off','resize','off');
+S.pt1 = uicontrol('style','text','unit','normalized','fontsize',14,'position',[0 .8 1 .2],'string','Please enter names of players.','backgroundc',[0.94 .94 .94]);
+
+S.pt1 = uicontrol('style','text','unit','normalized','fontsize',14,'position',[0 .6 .3 .2],'string','Player 1','backgroundc',[0.94 .94 .94]);
+S.pt2 = uicontrol('style','text','unit','normalized','fontsize',14,'position',[0 .4 .3 .2],'string','Player 2','backgroundc',[0.94 .94 .94]);
+
+S.pe1 = uicontrol('style','edit','unit','normalized','fontsize',14,'position',[.3 .6 .4 .2],'string','','backgroundc',[0.94 .94 .94]);
+S.pe2 = uicontrol('style','edit','unit','normalized','fontsize',14,'position',[.3 .4 .4 .2],'string','','backgroundc',[0.94 .94 .94]);
+
+S.pp1 = uicontrol('style','popupmenu','unit','normalized','fontsize',14,'position',[.7 .6 .3 .2],'string',{'Red' 'Blue' 'Yellow' 'White' 'Green' 'Orange'},'backgroundc',[0.94 .94 .94]);
+S.pp2 = uicontrol('style','popupmenu','unit','normalized','fontsize',14,'position',[.7 .4 .3 .2],'string',{'Red' 'Blue' 'Yellow' 'White' 'Green' 'Orange'},'backgroundc',[0.94 .94 .94]);
+
+S.pb = uicontrol('style','push','unit','normalized','fontsize',14,'position',[.3 0 .4 .25],'string','Okay','backgroundc',[0.94 .94 .94],'callback',{@twoProcess});
+
+end %function [] = playTwo(~,~)
+
+function [] = twoProcess(~,~)
+
+name1=get(S.pe1,'string');
+name2=get(S.pe2,'string');
+
+color1=get(S.pp1,'value');
+if color1 == 1
+   color1=[1 0 0];%Red
+elseif color1 == 2
+   color1=[0 0 1];%Blue
+elseif color1 == 3
+   color1=[1 1 0];%Yellow
+elseif color1 == 4
+   color1=[1 1 1];%White
+elseif color1 == 5
+   color1=[0 1 0];%Green
+elseif color1== 6
+   color1=[1 .5 0];%Orange
+end %if color1 == 1
+
+color2=get(S.pp2,'value');
+if color2 == 1
+   color2=[1 0 0];%Red
+elseif color2 == 2
+   color2=[0 0 1];%Blue
+elseif color2 == 3
+   color2=[1 1 0];%Yellow
+elseif color2 == 4
+   color2=[1 1 1];%White
+elseif color2 == 5
+   color2=[0 1 0];%Green
+elseif color2== 6
+   color2=[1 .5 0];%Orange
+end %if color2 == 1
+
 playerCount=2;
-n={name1 name2};
+n={name1 name2 color1 color2};
 
 close all
 
-
 Memory(matSize,playerCount,n)
 
-end
+end %function [] = twoProcess(~,~)
 
 %-----------------Try Again-----------------
 function [] = tryagain(~,~,p,name) %used to restart game, on same difficulty
     
 close all
 Memory(matSize,p,name)
-end
+end 
 
 
 %-----------------Easy Medium Hard-----------------
@@ -306,6 +362,7 @@ function [] = easy(~,~,p,name)
 close all
 Memory(3,p,name)
 end
+
 
 function [] = medium(~,~,p,name)
 close all
@@ -319,9 +376,9 @@ end
 
 %-----------------Creates a custom size of a board-------------------------
 function [] = custom(~,~,p,name) 
-itext1= 'Please Enter a Number for a Custom Game, 1 is easiest.';
+itext1= 'Please Enter a Number for a Custom Game, 1 is easiest.  The number entered is the number of rows and columns created, to create square playing field.';
 itext2= '*Note that some sizes may require the window to be resized, or may not work for your computer.';
-S.fh = figure('units','normalized','position',[.3 .3 .25 .25],'menubar','none','name','Custom Options','numbertitle','off','resize','off');
+S.fh = figure('units','normalized','position',[.3 .3 .3 .3],'menubar','none','name','Custom Options','numbertitle','off','resize','on');
 S.pt1 = uicontrol('style','text','unit','normalized','fontsize',14,'position',[0 .7 1 .3],'string',itext1,'backgroundc',[0.94 .94 .94]);
 S.pt2 = uicontrol('style','text','unit','normalized','fontsize',10,'position',[0 .5 1 .2],'string',itext2,'backgroundc',[0.94 .94 .94]);
 S.pe = uicontrol('style','edit','unit','normalized','fontsize',14,'position',[.3 .2 .4 .2],'string','','backgroundc',[0.94 .94 .94],'callback',{@cust,p,name});
@@ -336,16 +393,16 @@ end %function [] = cust(~,~)
 end %function [] = custom(~,~)
 
 function [] = help_play(~,~)
-itext1= '1 Player games, simply try to match the left side of the board with the right side board.  2 Player game Bla bla bla';
-S.fhelp = figure('units','normalized','position',[.3 .3 .25 .25],'menubar','none','name','How To Play','numbertitle','off','resize','off');
+itext1= '1 Player games, simply try to match the left side of the board with the right side board.  2 Player game, match left side with right side.  Correct attempts allow the user to contiue to play.  Incorrect attempts, current player loses the turn and switches to the other player';
+S.fhelp = figure('units','normalized','position',[.3 .3 .25 .25],'menubar','none','name','How To Play','numbertitle','off','resize','on');
 S.pt1 = uicontrol('style','text','unit','normalized','fontsize',15,'position',[0 0 1 1],'string',itext1,'backgroundc',[0.94 .94 .94]);
-end
+end %function [] = help_play(~,~)
 
 function [] = about(~,~)
-itext1= 'This game was created by: Kenny Ben Isaac and Nate';
-S.fhelp = figure('units','normalized','position',[.3 .3 .25 .25],'menubar','none','name','How To Play','numbertitle','off','resize','off');
+itext1= 'This game was created by: Kenny Ben Isaac and Nate on 2016';
+S.fhelp = figure('units','normalized','position',[.3 .3 .25 .25],'menubar','none','name','How To Play','numbertitle','off','resize','on');
 S.pt1 = uicontrol('style','text','unit','normalized','fontsize',15,'position',[0 0 1 1],'string',itext1,'backgroundc',[0.94 .94 .94]); 
-end
+end %function [] = about(~,~)
 
 end %function [] = Memory(~,~)
 
